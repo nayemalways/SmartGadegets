7621e0de9963fdb229d8cc94bb42249ee46e1a02
Nayem Ahmed <nishanahmed13913@gmail.com>
2025-06-01 03:12:16 +0600

    added routing for applications pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter,  Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/contact' element={<Contact />} />
          </Routes>
        </BrowserRouter>
     </ >
